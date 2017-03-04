23347bb452192646ce024af7d21b97aa8e1199f3
Jean-Philippe Sirois <jp@veksen.net>
2017-03-03 21:12:34 -0500

    make the home page show builds

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,36 @@
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const Build = require('../models/Build');
+
+const splitArrayIntoGroups = require('../helpers/array').splitArrayIntoGroups;
+
+Promise.promisifyAll(mongoose);
+
 /**
  * GET /
  * Home page.
  */
 exports.index = (req, res) => {
-  res.render('home', {
+  let query = {
+    $or: [
+      { draft: false },
+      { draft: { $exists: false } }
+    ]
+  };
+
+  Promise.props({
     path: 'home',
-    title: 'Home'
+    title: 'Home',
+    builds: Build
+      .find(query)
+      .execAsync()
+  })
+  .then(function(results) {
+    results.builds = splitArrayIntoGroups(results.builds, 3);
+
+    res.render('home', results);
+  })
+  .catch(function(err) {
+    res.send(500);
   });
 };
