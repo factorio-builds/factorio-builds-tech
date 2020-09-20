53afb785b766bdca7701f36d2678e581c39865f5
Jean-Philippe Sirois <jp@veksen.net>
2020-09-20 16:47:28 -0400

    refact: extract global styles and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,8 @@
 import type { AppProps } from "next/app"
 import "./globalStyles.css"
-import { createGlobalStyle, ThemeProvider } from "styled-components"
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-      sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
-      monospace;
-  }
-
-  body,
-  #__next {
-    min-height: 100vh;
-  }
-
-  #__next {
-    display: flex;
-    flex-direction: column;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: "#0070f3",
-  },
-}
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "../styles/global-style"
+import { theme } from "../styles/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ styles/global-style.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+
+  body,
+  #__next {
+    min-height: 100vh;
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+  }
+`
