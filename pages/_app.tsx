aed1cd2549fe5832bc19c6a5d15c031edb8c70f5
Jean-Philippe Sirois <jp@veksen.net>
2020-09-20 16:48:20 -0400

    refact: clean up global css in favor of styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from "next/app"
-import "./globalStyles.css"
 import { ThemeProvider } from "styled-components"
 import { GlobalStyle } from "../styles/global-style"
 import { theme } from "../styles/theme"
