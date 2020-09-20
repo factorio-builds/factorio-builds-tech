7454c701882fe98e8933727172a83bf1234754a3
Jean-Philippe Sirois <jp@veksen.net>
2020-09-19 21:45:58 -0400

    fix: make sure styled-components work over SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,48 @@
 import type { AppProps } from "next/app"
 import "./globalStyles.css"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+
+  body,
+  #__next {
+    min-height: 100vh;
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
