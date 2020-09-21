682d3767e74468534a5b2cfc9f84e19e6a0aa409
Jean-Philippe Sirois <jp@veksen.net>
2020-09-21 19:27:59 -0400

    feat: add link to create build form in header

--- components/Header/header.component.tsx
+++ components/Header/header.component.tsx
@@ -1,11 +1,17 @@
 import React from "react"
+import Link from "next/link"
 import Container from "../Container"
 import * as SC from "./header.styles"
 
 function Header(): JSX.Element {
   return (
     <SC.HeaderWrapper>
-      <Container>my blueprints</Container>
+      <Container>
+        <Link href="/">my blueprints</Link>
+        <Link href="/builds/create">
+          <SC.CreateBuildButton>Add a build</SC.CreateBuildButton>
+        </Link>
+      </Container>
     </SC.HeaderWrapper>
   )
 }
--- components/Header/header.styles.tsx
+++ components/Header/header.styles.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { ContainerWrapper as Container } from "../Container/container.styles"
 
 export const HeaderWrapper = styled.div`
   display: flex;
@@ -6,4 +7,24 @@ export const HeaderWrapper = styled.div`
   background: #8fce5b;
   height: 74px;
   padding: 0 20px;
+
+  ${Container} {
+    align-items: center;
+  }
+`
+
+export const CreateBuildButton = styled.button`
+  background: linear-gradient(180deg, #7950b9 0%, #543b7a 100%);
+  color: #ffffff;
+  border: none;
+  border-radius: 5px;
+  font-size: 17px;
+  line-height: 38px;
+  padding: 0 14px;
+  cursor: pointer;
+  margin-left: auto;
+
+  &:hover {
+    background: linear-gradient(180deg, #613d99 0%, #3d2b59 100%);
+  }
 `
