fd6e73821c34926221bc1119122709142e5fa3a1
Jean-Philippe Sirois <jp@veksen.net>
2020-09-22 22:19:13 -0400

    bootstrap connecting to database from Next

--- db/models/index.js
+++ db/models/index.js
@@ -15,21 +15,17 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+const buildsModel = require('./builds');
+const usersModel = require('./users');
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+db.builds = buildsModel(sequelize, Sequelize.DataTypes);
+db.users = usersModel(sequelize, Sequelize.DataTypes);
+
+// Object.keys(db).forEach((modelName) => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db)
+//   }
+// })
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
