cb4a348cd3d30386ee2a21f723d0531f77392a3f
Ken Perkins <ken.perkins@rackspace.com>
2020-09-28 10:01:52 -0700

    Adding build icon support

---
+++ utils/image-downloader.js
@@ -0,0 +1,27 @@
+"use strict"
+const fs = require("fs")
+const request = require("request")
+const path = require("path")
+
+let jsonData = JSON.parse(fs.readFileSync("utils/image-sources.json", "utf-8"))
+
+const sizes = ["small", "medium", "large"].forEach((size) => {
+  jsonData[size].forEach((img) => {
+    var src = jsonData.base_path + img
+    var filename = path.basename(img).toLowerCase()
+    var dest = filename
+
+    const formats = ["32px-", "48px-"].forEach((px) => {
+      if (filename.indexOf(px) != -1) {
+        dest = filename.substr(px.length)
+      }
+    })
+
+    dest = dest.replace(/_/g, "-")
+
+    console.log(src + " => " + dest)
+    request(src).pipe(
+      fs.createWriteStream("./public/img/icons/" + size + "/" + dest)
+    )
+  })
+})
