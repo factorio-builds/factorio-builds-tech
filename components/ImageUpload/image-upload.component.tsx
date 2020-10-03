a9c86bbf76dec14f733c7c6611b7987df52038ba
Jean-Philippe Sirois <jp@veksen.net>
2020-10-03 08:52:29 -0400

    feat: bootstrap image upload on the front-end

---
+++ components/ImageUpload/image-upload.component.tsx
@@ -0,0 +1,75 @@
+import cx from "classnames"
+import React, { useCallback, useState } from "react"
+import { useDropzone } from "react-dropzone"
+import * as SC from "./image-upload.styles"
+
+interface IImageUploadProps {
+  onChange: (file: File | null) => void
+}
+
+function toImage(buffer: ArrayBuffer) {
+  const arrayBufferView = new Uint8Array(buffer)
+  const blob = new Blob([arrayBufferView], { type: "image/jpeg" })
+  // @ts-ignore
+  const urlCreator = window.URL || window.webkitURL!
+  return urlCreator.createObjectURL(blob)
+}
+
+function ImageUpload(props: IImageUploadProps): JSX.Element {
+  const [imagePreview, setImagePreview] = useState<string | null>(null)
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const [file] = acceptedFiles
+
+    const reader = new FileReader()
+
+    reader.onabort = () => console.log("file reading was aborted")
+    reader.onerror = () => console.log("file reading has failed")
+    reader.onload = () => {
+      const binaryStr = reader.result
+      const imageUrl = toImage(binaryStr as ArrayBuffer)
+      console.log(imageUrl)
+      setImagePreview(imageUrl)
+      props.onChange(file)
+    }
+    reader.readAsArrayBuffer(file)
+  }, [])
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    multiple: false,
+  })
+
+  function removeImage() {
+    setImagePreview(null)
+    props.onChange(null)
+  }
+
+  return (
+    <SC.ImageUploadWrapper
+      className={cx({ "is-active": isDragActive, "has-image": imagePreview })}
+    >
+      <SC.UploadZone {...getRootProps()}>
+        <input {...getInputProps()} />
+        {imagePreview ? (
+          <SC.ImagePreview src={imagePreview} />
+        ) : (
+          <React.Fragment>
+            <SC.StyledPlusIcon color="#67469b" />
+            <SC.Hint>
+              {isDragActive ? "(drop image here)" : "(click or drag image)"}
+            </SC.Hint>
+          </React.Fragment>
+        )}
+      </SC.UploadZone>
+
+      <SC.DeleteButtonWrapper>
+        {imagePreview && (
+          <SC.DeleteButton onClick={removeImage}>delete image</SC.DeleteButton>
+        )}
+      </SC.DeleteButtonWrapper>
+    </SC.ImageUploadWrapper>
+  )
+}
+
+export default ImageUpload
---
+++ icons/plus.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+interface IPlusProps {
+  color?: string
+}
+
+export const Plus: React.FC<IPlusProps> = ({
+  color = "#000",
+  ...restProps
+}) => (
+  <svg {...restProps} fill="none" viewBox="0 0 96 100">
+    <path
+      stroke={color}
+      strokeLinecap="round"
+      strokeWidth="4"
+      d="M2-2h95.36"
+      transform="matrix(.003 1 -1 .00327 47.89 .32)"
+    />
+    <path
+      stroke={color}
+      strokeLinecap="round"
+      strokeWidth="4"
+      d="M93.63 51.5H2.37"
+    />
+  </svg>
+)
