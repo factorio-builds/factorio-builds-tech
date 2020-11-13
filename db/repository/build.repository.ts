faab63b6f23d988172edee50e4cf0f582cd07765
Jean-Philippe Sirois <jp@veksen.net>
2020-11-13 18:10:43 -0500

    feat: validate build ownership on update

--- db/repository/build.repository.ts
+++ db/repository/build.repository.ts
@@ -1,8 +1,16 @@
 import { Repository } from "typeorm"
+import { EntityRepository } from "typeorm"
 import { Build } from "../entities/build.entity"
 import { ensureConnection } from "../index"
 
-export async function BuildRepository(): Promise<Repository<Build>> {
+@EntityRepository(Build)
+class _BuildRepository extends Repository<Build> {
+  async isOwnedBy(buildId: string, ownerId: string) {
+    return this.findOne({ id: buildId, owner: { id: ownerId } }).then(Boolean)
+  }
+}
+
+export async function BuildRepository(): Promise<_BuildRepository> {
   const connection = await ensureConnection()
-  return connection!.getRepository(Build)
+  return connection!.getCustomRepository(_BuildRepository)
 }
