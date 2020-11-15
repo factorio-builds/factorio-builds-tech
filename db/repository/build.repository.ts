b03ec4639b7f95ca8a74d04c6b10c9277766d41f
Jean-Philippe Sirois <jp@veksen.net>
2020-11-15 04:00:22 -0500

    feat: track view count

--- db/repository/build.repository.ts
+++ db/repository/build.repository.ts
@@ -8,6 +8,15 @@ class _BuildRepository extends Repository<Build> {
   async isOwnedBy(buildId: string, ownerId: string) {
     return this.findOne({ id: buildId, owner: { id: ownerId } }).then(Boolean)
   }
+
+  async incrementViews(build: Build): Promise<boolean> {
+    const updateResult = await this.update(
+      { id: build.id },
+      { views: build.views + 1 }
+    )
+
+    return Boolean(updateResult.affected)
+  }
 }
 
 export async function BuildRepository(): Promise<_BuildRepository> {
---
+++ server/redis.ts
@@ -0,0 +1,3 @@
+import redis from "redis"
+
+export const redisClient = redis.createClient()
