d2c10569497665c6359556c06831970291af8a8c
Jean-Philippe Sirois <jp@veksen.net>
2021-02-17 16:18:28 -0500

    feat: make tags collapsable

--- frontend/components/pages/BuildFormPage/step-2-data.component.tsx
+++ frontend/components/pages/BuildFormPage/step-2-data.component.tsx
@@ -1,16 +1,76 @@
-import React from "react"
+import React, { useCallback, useMemo, useState } from "react"
 import { Field, FormikProps } from "formik"
+import Caret from "../../../icons/caret"
 import tags from "../../../tags.json"
 import Input from "../../form/FormikInputWrapper"
 import InputGroup from "../../form/InputGroup"
 import Stacker from "../../ui/Stacker"
 import { IFormValues } from "./build-form-page.component"
+import * as SC from "./build-form-page.styles"
+
+interface ICollapsableGroupProps {
+  group: string
+  groupTags: string[]
+  isCollapsable: (tags: string[]) => boolean
+}
 
 interface IStep2DataProps {
   formikProps: FormikProps<IFormValues>
 }
 
-const Step2Data: React.FC<IStep2DataProps> = () => {
+const CollapsableGroup = (props: ICollapsableGroupProps): JSX.Element => {
+  const groupsToTags = useMemo(
+    () => props.groupTags.map((tag) => `/${props.group}/${tag}`),
+    [props.groupTags]
+  )
+  const isCollapsable = props.isCollapsable(groupsToTags)
+
+  const [collapsed, setCollapsed] = useState(true)
+
+  const expand = useCallback(() => {
+    if (isCollapsable) {
+      setCollapsed((prevCollapsed) => !prevCollapsed)
+    }
+  }, [collapsed, isCollapsable])
+
+  return (
+    <InputGroup
+      key={props.group}
+      legend={
+        <SC.GroupTitle onClick={expand}>
+          <Caret inverted={collapsed} />
+          {props.group}
+        </SC.GroupTitle>
+      }
+    >
+      {!collapsed && (
+        <Stacker gutter={4}>
+          {props.groupTags.map((tag) => {
+            return (
+              <Field
+                key={tag}
+                name="tags"
+                label={tag}
+                type="checkbox"
+                value={`/${props.group}/${tag}`}
+                component={Input}
+              />
+            )
+          })}
+        </Stacker>
+      )}
+    </InputGroup>
+  )
+}
+
+const Step2Data: React.FC<IStep2DataProps> = (props) => {
+  const isCollapsable = useCallback(
+    (tags: string[]) => {
+      return !tags.some((tag) => props.formikProps.values.tags.includes(tag))
+    },
+    [props.formikProps.values.tags]
+  )
+
   return (
     <>
       <Field
@@ -31,30 +91,22 @@ const Step2Data: React.FC<IStep2DataProps> = () => {
         component={Input}
       />
 
-      <Stacker gutter={8}>
-        {Object.keys(tags).map((tagCategory) => {
-          const tagGroup = tags[tagCategory as keyof typeof tags]
+      <InputGroup legend="Tags">
+        <Stacker gutter={8}>
+          {Object.keys(tags).map((tagCategory) => {
+            const tagGroup = tags[tagCategory as keyof typeof tags]
 
-          return (
-            <InputGroup key={tagCategory} legend={tagCategory}>
-              <Stacker gutter={4}>
-                {tagGroup.map((tag) => {
-                  return (
-                    <Field
-                      key={tag}
-                      name="tags"
-                      label={tag}
-                      type="checkbox"
-                      value={`/${tagCategory}/${tag}`}
-                      component={Input}
-                    />
-                  )
-                })}
-              </Stacker>
-            </InputGroup>
-          )
-        })}
-      </Stacker>
+            return (
+              <CollapsableGroup
+                key={tagCategory}
+                group={tagCategory}
+                groupTags={tagGroup}
+                isCollapsable={isCollapsable}
+              />
+            )
+          })}
+        </Stacker>
+      </InputGroup>
     </>
   )
 }
