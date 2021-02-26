fc5500f656a44ebef9c220039a552d819f4cd6f2
Jean-Philippe Sirois <jp@veksen.net>
2021-02-26 03:49:06 -0500

    refact: clean up unused heuristics

--- frontend/utils/blueprint-heuristics.ts
+++ frontend/utils/blueprint-heuristics.ts
@@ -5,8 +5,6 @@ interface IComputedHeuristic {
   confidence: number
 }
 
-type THeuristics = Record<string, IComputedHeuristic>
-
 function hasEntity(
   blueprint: IBlueprint,
   entityName: IBlueprintEntity["name"]
@@ -60,6 +58,7 @@ function hasEntityWithRecipe(
   })
 }
 
+// TODO: reimplement
 // TODO: could be improved to check that the combinator is right next to a belt facing it
 // odds are 99% of cases, a combinator with an item indicates input
 export function withMarkedInputs(blueprint: IBlueprint): IComputedHeuristic {
@@ -82,76 +81,6 @@ export function withMarkedInputs(blueprint: IBlueprint): IComputedHeuristic {
   return {
     value: output,
     confidence: 1,
-  }
-}
-
-// can blindly assume that anything with some sort of train piece can be flagged as train
-// only exception are train cargos used as a box
-function isTrains(blueprint: IBlueprint): IComputedHeuristic {
-  if (!blueprint.entities) {
-    return {
-      value: false,
-      confidence: 1,
-    }
-  }
-
-  const output = blueprint.entities.some((entity) => {
-    return (
-      entity.name === "straight-rail" ||
-      entity.name === "curved-rail" ||
-      entity.name === "train-stop" ||
-      entity.name === "rail-signal" ||
-      entity.name === "rail-chain-signal"
-    )
-  })
-
-  return {
-    value: output,
-    confidence: 1,
-  }
-}
-
-export function withBeacons(blueprint: IBlueprint): IComputedHeuristic {
-  if (!blueprint.entities) {
-    return {
-      value: false,
-      confidence: 1,
-    }
-  }
-
-  const output = blueprint.entities.some((entity) => {
-    return entity.name === "beacon"
-  })
-
-  return {
-    value: output,
-    confidence: 1,
-  }
-}
-
-// TODO: review/observe performance over time
-export function blueprintHeuristics(blueprint?: IBlueprint): THeuristics {
-  if (!blueprint) {
-    return {
-      isTrains: {
-        value: false,
-        confidence: 1,
-      },
-      withMarkedInputs: {
-        value: false,
-        confidence: 1,
-      },
-      withBeacons: {
-        value: false,
-        confidence: 1,
-      },
-    }
-  }
-
-  return {
-    isTrains: isTrains(blueprint),
-    withMarkedInputs: withMarkedInputs(blueprint),
-    withBeacons: withBeacons(blueprint),
   }
 }
 
