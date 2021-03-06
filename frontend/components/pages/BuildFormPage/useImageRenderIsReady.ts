0c441e91bdd4e2bb7bcf936d9ab6d69dd33ed856
Jean-Philippe Sirois <jp@veksen.net>
2021-03-06 17:33:58 -0500

    fix: now possible to create a book with custom image

--- frontend/components/pages/BuildFormPage/useImageRenderIsReady.ts
+++
@@ -1,9 +0,0 @@
-import useImage from "../../../hooks/useImage"
-
-function useImageRenderIsReady(src?: string) {
-  const { loaded } = useImage(src)
-
-  return loaded
-}
-
-export default useImageRenderIsReady
