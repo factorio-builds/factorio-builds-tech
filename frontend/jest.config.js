e7f5f631f4f509f8f925eaccc8b1ed9e07076337
Jean-Philippe Sirois <jp@veksen.net>
2021-03-24 00:45:34 -0400

    tests: add tests for WithIcons component

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,7 @@
 /* global module */
 
 module.exports = {
+  setupFiles: ["<rootDir>/jest.setup.js"],
   testRegex: "/__tests__/.*.test.tsx?$",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
 }
